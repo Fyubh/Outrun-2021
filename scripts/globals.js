f9d11d33a0fc765894ed2fa099a32fd18153ecb4
Zhangir <zhangirban@gmail.com>
2021-02-10 22:17:57 +0600

    beta 1.0

--- scripts/globals.js
+++ scripts/globals.js
@@ -1,12 +1,5 @@
 var CAMD = 0.84;
-var WIDTH;
-var HEIGHT;
+var WIDTH = 1920;
+var HEIGHT = 946;
 var ROADW = 2000;
 var SEGL = 200;
-var HEALTH = 1000;
-var SCORE = 0;
-var SPEED = 400;
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
