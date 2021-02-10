dcab674f8e526e96d6d37d12335d6facbb5ce8ec
Fyubh <zhangirban@gmail.com>
2021-02-10 22:11:32 +0600

    Magadan2077

--- scripts/globals.js
+++ scripts/globals.js
@@ -1,5 +1,12 @@
 var CAMD = 0.84;
-var WIDTH = 1920;
-var HEIGHT = 946;
+var WIDTH;
+var HEIGHT;
 var ROADW = 2000;
 var SEGL = 200;
+var HEALTH = 1000;
+var SCORE = 0;
+var SPEED = 400;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
